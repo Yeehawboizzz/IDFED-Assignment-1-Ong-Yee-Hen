16ad38c5f790300b85447900727c27e7e68dcf56
Yeehawboizzz <s10258759@connect.np.edu.sg>
2023-11-30 00:57:11 +0800

    Created new JS for one new function to hover info

---
+++ OurSpaceJS.js
@@ -0,0 +1,12 @@
+
+// function to show info when hover
+function showInstructorInfo(box) {
+    const instructorInfo = box.querySelector('.instructor-info');
+    instructorInfo.style.display = 'block';
+}
+
+// function to hide when mouse out of box
+function hideInstructorInfo(box) {
+    const instructorInfo = box.querySelector('.instructor-info');
+    instructorInfo.style.display = 'none';
+}
